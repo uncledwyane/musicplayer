5b53ebd44dd7dc109dfe28d5a6ae2ff244eb60ba
wangbo <327656879@qq.com>
2021-01-07 14:15:05 +0800

    more

---
+++ src/components/component/TrackPlay.vue
@@ -0,0 +1,82 @@
+<template>
+    <div id="trackPlay">
+        <div class="top" @click="hide">
+            <div class="backButton">
+                <button>《</button>
+            </div>
+            <div class="trackName">trackName</div>
+            <div class="trackInfo">...</div>
+        </div>
+        <div class="coverInfo">
+            <div class="coverImg">
+                <img
+                    src="https://p1.music.126.net/5uRGDj93JD23p4ejcPPQhA==/109951164151161218.jpg"
+                    alt=""
+                    class="cover"
+                />
+            </div>
+            <div class="trackNameAndArtist">
+                <p class="trackName">TrackName</p>
+                <p class="trackArtist">trackArtist</p>
+            </div>
+            <!--TODO 歌词-->
+        </div>
+        <div class="bottomControl">
+            <div class="trackProgress">
+                <div class="trackTime">
+                    <span class="playTime"></span>
+                    <span class="totalTime"></span>
+                </div>
+            </div>
+            <div class="playControl">
+                <div class="previoursTrack"></div>
+                <div class="pauseOrPlay"></div>
+                <div class="nextTrack"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import bus from "@/components/bus";
+export default {
+    data() {
+        return {
+            coverUrl: "",
+            trackName: "",
+            trackArtist: "",
+            trackTotalTime: "",
+        };
+    },
+    methods: {
+        hide() {
+            bus.$emit("setTrackPlayStatus", false);
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+#trackPlay {
+    width: 100%;
+    height: 100%;
+    .top {
+        width: 100%;
+        height: 5%;
+        background: lightblue;
+    }
+    .coverInfo {
+        width: 100%;
+        height: 55%;
+        background: rgb(240, 241, 243);
+    }
+    .bottomControl {
+        width: 100%;
+        height: 45%;
+        background: rgb(255, 255, 255);
+        border-radius: 30px 30px 0 0;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+        transform: translateY(-5%);
+    }
+}
+</style>
--- src/components/page/PlayDetail.vue
+++ src/components/page/PlayDetail.vue
@@ -1,46 +1,60 @@
 <template>
-  <div class="playDetailWrap">
-    <div
-      class="playlistInfo"
-      ref="playlistInfo"
-      :style="{
-        background: 'url(' + coverImgUrl + ')',
-        backgroundSize: 'contain',
-        backgroundRepeat: 'no-repeat',
-      }"
-    >
-      <div class="cover"><img :src="coverImgUrl" alt="" class="coverImg" /></div>
-      <div class="creator">
-        <h3 class="playlistName">{{ name }}</h3>
-        <p class="creatorName">
-          <img :src="creator.avatarUrl" class="creatorHeader" /> {{ creator.nickname }}
-        </p>
-        <p class="playlistDescription">{{ description }}</p>
-      </div>
-    </div>
-    <div class="playlistTracks">
-      <div class="tracksWrap">
-        <mu-button color="red" round class="playAllButton" @click="hidePlayPreview"
-          ><mu-icon value="play_arrow"></mu-icon>播放全部</mu-button
-        >
+    <div class="playDetailWrap">
         <div
-          class="track"
-          v-for="(track, index) in tracks"
-          :key="track.id"
-          @click="sendTrackInfoToPlay(track)"
+            class="playlistInfo"
+            ref="playlistInfo"
+            :style="{
+                background: 'url(' + coverImgUrl + ')',
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+            }"
         >
-          <div class="index">{{ index + 1 }}</div>
-          <div class="trackInfo">
-            <h4 class="trackName">{{ track.name }}</h4>
-            <p class="artistName">{{ track.ar | formatArtist(track.ar) }}</p>
-          </div>
-          <div class="playAction">
-            <img src="../../../assets/img/Play.png" class="playStatus" />
-          </div>
+            <div class="blurBg"></div>
+            <div class="cover"><img :src="coverImgUrl" alt="" class="coverImg" /></div>
+            <div class="creator">
+                <h3 class="playlistName">{{ name }}</h3>
+                <p class="creatorName">
+                    <img :src="creator.avatarUrl" class="creatorHeader" />
+                    {{ creator.nickname }}
+                </p>
+                <p class="playlistDescription">{{ description }}</p>
+            </div>
         </div>
-      </div>
+        <div class="playlistTracks">
+            <div class="tracksWrap">
+                <mu-button color="red" round class="playAllButton"
+                    ><mu-icon value="play_arrow"></mu-icon>播放全部</mu-button
+                >
+                <div
+                    class="track"
+                    :class="
+                        track.id == songIsPlaying.songId && songIsPlaying.isPlaying
+                            ? 'track_playing'
+                            : ''
+                    "
+                    v-for="(track, index) in tracks"
+                    :key="track.id"
+                    @click="sendTrackInfoToPlay(track)"
+                >
+                    <div class="index">{{ index + 1 }}</div>
+                    <div class="trackInfo">
+                        <h4 class="trackName">{{ track.name }}</h4>
+                        <p class="artistName">{{ track.ar | formatArtist(track.ar) }}</p>
+                    </div>
+                    <div class="playAction" v-show="track.id == songIsPlaying.songId">
+                        <img
+                            :src="
+                                songIsPlaying.isPlaying
+                                    ? require('../../../assets/icon/pause.svg')
+                                    : require('../../../assets/icon/play.svg')
+                            "
+                            class="playStatus"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -48,196 +62,211 @@ import RestAPI from "@/api/restAPI";
 const restAPI = new RestAPI();
 import bus from "@/components/bus";
 export default {
-  data() {
-    return {
-      playId: "",
-      creator: "",
-      subscribers: "",
-      tracks: "",
-      coverImgUrl: "",
-      playCount: 0,
-      tags: [],
-      description: "",
-      name: "",
-      privileges: "",
-      iconImg: "assets/img/Play.png",
-      songIsPlaying: {
-        songId: "",
-        songStatus: {
-          0: "pause",
-          1: "playing",
+    data() {
+        return {
+            playId: "",
+            creator: "",
+            subscribers: "",
+            tracks: "",
+            coverImgUrl: "",
+            playCount: 0,
+            tags: [],
+            description: "",
+            name: "",
+            privileges: "",
+            iconImg: "assets/img/Play.png",
+            songIsPlaying: {
+                songId: "",
+                songName: "",
+                isPlaying: false,
+            },
+        };
+    },
+    filters: {
+        formatArtist(artists) {
+            var artistNames = [];
+            for (var i = 0; i < artists.length; i++) {
+                artistNames.push(artists[i].name);
+            }
+            return artistNames.toString();
         },
-      },
-    };
-  },
-  filters: {
-    formatArtist(artists) {
-      var artistNames = [];
-      for (var i = 0; i < artists.length; i++) {
-        artistNames.push(artists[i].name);
-      }
-      return artistNames.toString();
     },
-  },
-  created() {
-    var self = this;
-    self.playId = self.$route.params.id;
-    restAPI.getPlayListDetail(self.playId).then(function (res) {
-      self.creator = res.playlist.creator;
-      self.subscribers = res.playlist.subscribers;
-      self.tracks = res.playlist.tracks;
-      self.tags = res.playlist.tags;
-      self.description = res.playlist.description;
-      self.name = res.playlist.name;
-      self.coverImgUrl = res.playlist.coverImgUrl;
-      self.playCount = res.playlist.playCount;
-      self.privileges = res.privileges;
-    });
-  },
-  methods: {
-    sendTrackInfoToPlay(track) {
-      var songObj = {
-        songId: track.id,
-        songName: track.name,
-        isPlaying: true,
-      };
-      bus.$emit("pushTrackInfo", track);
-      bus.$emit("setPlayPreviewStatus", true);
-      // 更新正在播放的歌曲
-      bus.$emit("setPlayingSong", songObj);
+    created() {
+        var self = this;
+        self.playId = self.$route.params.id;
+        restAPI.getPlayListDetail(self.playId).then(function (res) {
+            self.creator = res.playlist.creator;
+            self.subscribers = res.playlist.subscribers;
+            self.tracks = res.playlist.tracks;
+            self.tags = res.playlist.tags;
+            self.description = res.playlist.description;
+            self.name = res.playlist.name;
+            self.coverImgUrl = res.playlist.coverImgUrl;
+            self.playCount = res.playlist.playCount;
+            self.privileges = res.privileges;
+        });
     },
-    hidePlayPreview() {
-      bus.$emit("setPlayPreviewStatus", false);
+    mounted() {
+        var self = this;
+        bus.$on("pauseOrPlay", function (value) {
+            if (value == "pause") {
+                self.songIsPlaying.isPlaying = false;
+            } else if (value == "play") {
+                self.songIsPlaying.isPlaying = true;
+            } else {
+            }
+        });
     },
-  },
+    methods: {
+        sendTrackInfoToPlay(track) {
+            var self = this;
+            var songObj = {
+                songId: track.id,
+                songName: track.name,
+                isPlaying: true,
+            };
+            bus.$emit("pushTrackInfo", track);
+            bus.$emit("setPlayPreviewStatus", true);
+            // 更新正在播放的歌曲
+            bus.$emit("setPlayingSong", songObj);
+            // 设置正在播放的歌曲
+            self.songIsPlaying.songId = songObj.songId;
+            self.songIsPlaying.songName = songObj.songName;
+            self.songIsPlaying.isPlaying = songObj.isPlaying;
+        },
+    },
 };
 </script>
 
 <style lang="scss" scoped>
 #playDetailWrap {
-  width: 100%;
-  height: 100%;
-  padding-bottom: 50px;
+    width: 100%;
+    height: 100%;
+    padding-bottom: 50px;
 }
 
 .playlistInfo {
-  width: 100%;
-  display: flex;
-  padding: 20px;
+    width: 100%;
+    display: flex;
+    padding: 20px;
 }
 .cover {
-  width: 30%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    width: 30%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 
 .creator {
-  width: 70%;
+    width: 70%;
 }
 
 .coverImg {
-  width: 70px;
-  height: 70px;
-  border-radius: 10px;
+    width: 70px;
+    height: 70px;
+    border-radius: 10px;
 }
 
 .creatorHeader {
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  margin-right: 5px;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    margin-right: 5px;
 }
 .playlistName {
-  overflow: hidden;
-  margin: 5px;
-  height: 20px;
+    overflow: hidden;
+    margin: 5px;
+    height: 20px;
 }
 .creatorName {
-  height: 20px;
-  margin: 0;
-  display: inline-flex;
-  line-height: 20px;
-  font-size: 12px;
-  align-items: center;
+    height: 20px;
+    margin: 0;
+    display: inline-flex;
+    line-height: 20px;
+    font-size: 12px;
+    align-items: center;
 }
 .playlistDescription {
-  font-size: 12px;
-  margin: 0;
-  overflow: hidden;
-  height: 53px;
-  display: flex;
-  justify-content: space-between;
+    font-size: 12px;
+    margin: 0;
+    overflow: hidden;
+    height: 53px;
+    display: flex;
+    justify-content: space-between;
 }
 p {
-  margin: 0;
+    margin: 0;
 }
 .playlistTracks {
-  width: 100%;
-  height: 80%;
-  position: relative;
-  margin-top: 50px;
-  padding-bottom: 80px;
+    width: 100%;
+    height: 80%;
+    position: relative;
+    margin-top: 50px;
+    padding-bottom: 130px;
 }
 .tracksWrap {
-  width: 90%;
-  background: #fff;
-  border-radius: 10px;
-  margin: 20px auto;
+    width: 90%;
+    background: #fff;
+    border-radius: 10px;
+    margin: 20px auto;
 }
 .track {
-  width: 100%;
-  height: 50px;
-  display: flex;
-  margin-bottom: 10px;
-  padding: 5px 0;
+    width: 100%;
+    height: 50px;
+    display: flex;
+    margin-bottom: 10px;
+    padding: 5px 0;
 }
 .track:hover {
-  background: lightgrey;
+    background: lightgrey;
 }
 .track:active {
-  background: lightgrey;
+    background: lightgrey;
 }
 .index {
-  width: 10%;
-  height: 40px;
-  text-align: center;
-  line-height: 40px;
-  color: grey;
+    width: 10%;
+    height: 40px;
+    text-align: center;
+    line-height: 40px;
+    color: grey;
 }
 .trackInfo {
-  width: 75%;
-  height: 40px;
-  overflow: hidden;
-  h4 {
-    margin: 0;
-  }
+    width: 75%;
+    height: 40px;
+    overflow: hidden;
+    h4 {
+        margin: 0;
+    }
 }
 .trackName {
-  width: 100%;
-  height: 28px;
-  line-height: 28px;
+    width: 100%;
+    height: 28px;
+    line-height: 28px;
 }
 .artistName {
-  width: 100%;
-  text-align: left;
-  height: 12px;
-  font-size: 12px;
-  line-height: 12px;
+    width: 100%;
+    text-align: left;
+    height: 12px;
+    font-size: 12px;
+    line-height: 12px;
 }
 .playAllButton {
-  position: absolute;
-  top: -40px;
-  right: 20px;
+    position: absolute;
+    top: -40px;
+    right: 20px;
 }
 .playAction {
-  width: 15%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  .playStatus {
-    width: 30px;
-    height: 30px;
-  }
+    width: 15%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .playStatus {
+        width: 30px;
+        height: 30px;
+    }
+}
+.track_playing {
+    color: rgb(255, 0, 0);
+    border-left: 5px solid red;
 }
 </style>
